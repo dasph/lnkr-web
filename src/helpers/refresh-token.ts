34a017fdeb9f99dd9fbe0c9fa6871678b91e7e91
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2024-06-08 20:45:42 +0200

    fix: refresh token interval

--- src/helpers/refresh-token.ts
+++ src/helpers/refresh-token.ts
@@ -8,5 +8,5 @@ let interval: NodeJS.Timeout | undefined = undefined
 const callback = () => fetchApi('auth/refresh', 'get', {}).catch(() => false).then((value) => setAuth(!!value))
 
 export const toogleRefreshToken = (stop?: boolean) => {
-  interval = (stop || interval) ? void clearInterval(interval) : setInterval(callback, 3_000)
+  interval = (stop || interval) ? void clearInterval(interval) : setInterval(callback, 1000 * 60 * 4.5)
 }
